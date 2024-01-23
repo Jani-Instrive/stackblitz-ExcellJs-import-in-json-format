b09b1a137515e23bfa8c2ed783d69c7359bcc825
Jani-Instrive <shaikjani@instrive.in>
2024-01-23 10:35:02 +0530

    Added basic button for form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { FC } from 'react';
-
+import {Box, Button, Grid, Typography} from '@mui/material'
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
+  const handleSubmit = (e) => {
+    console.log('e---->',e.target.value)
+  }
   return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Box>
+       <form onSubmit={handleSubmit}>
+          <Typography>Upload Excel file</Typography>
+          <input  type='file'
+            style={{ maxWidth: '500px', margin: 'auto' }} accept='.xlsx' className='form-control'
+             />
+          <Grid style={{ marginTop: "2%" }}>
+            <Button
+              type='submit' className='btn btn-success'
+            >Submit</Button>
+          </Grid>
+        </form>
+    </Box>
   );
 };
